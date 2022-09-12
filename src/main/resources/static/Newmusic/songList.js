3fc1adb37ebd96f659a19f7b3d62aad1f9762d96
PleaseDontBePanic <100511403+PleaseDontBePanic@users.noreply.github.com>
2022-09-12 15:11:18 +0800

    Add files via upload
    Add lyrics display

---
+++ src/main/resources/static/Newmusic/songList.js
@@ -0,0 +1,545 @@
+
+var songList = [
+    {
+        title : 'Intro',
+        singer : '周杰伦',
+        audio : '/music/data/Intro-周杰伦.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : ""
+    },
+    {
+        title : '最伟大的作品',
+        singer : '周杰伦',
+        audio : '/music/data/最伟大的作品-周杰伦.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/最伟大的作品.lrc'
+    },
+    {
+        title : '粉色海洋',
+        singer : '周杰伦',
+        audio : '/music/data/粉色海洋-周杰伦.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/粉色海洋.lrc'
+    },
+    {
+        title : '红颜如霜',
+        singer : '周杰伦',
+        audio : '/music/data/红颜如霜-周杰伦.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/红颜如霜.lrc'
+    },
+    {
+        title : '还在流浪',
+        singer : '周杰伦',
+        audio : '/music/data/还在流浪-周杰伦.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/还在流浪.lrc'
+    },
+    {
+        title : '错过的烟火',
+        singer : '周杰伦',
+        audio : '/music/data/错过的烟火-周杰伦.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/错过的烟火.lrc'
+    },
+    {
+        title : '迷迭香',
+        singer : '周杰伦',
+        audio : '/music/data/mdx.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/迷迭香.lrc'
+    },
+    {
+        title : '迷迭香',
+        singer : '周杰伦',
+        audio : '/music/data/mdx.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/迷迭香.lrc'
+    },
+    {
+        title : '安静',
+        singer : '周杰伦',
+        audio : '/music/data/龙卷风.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/安静.lrc'
+    },
+    {
+        title : '晴天',
+        singer : '周杰伦',
+        audio : '/music/data/qt.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/晴天.lrc'
+    },
+    {
+        title : '告白气球',
+        singer : '周杰伦',
+        audio : '/music/data/gb.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/告白气球.lrc'
+    },
+    {
+        title : 'Mojito',
+        singer : '周杰伦',
+        audio : '/music/data/moji.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/Mojito.lrc'
+    },
+    {
+        title : '青花瓷',
+        singer : '周杰伦',
+        audio : '/music/data/q.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/青花瓷.lrc'
+    },
+    {
+        title : '稻香',
+        singer : '周杰伦',
+        audio : '/music/data/dx.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/稻香.lrc'
+    },
+    {
+        title : '等你下课',
+        singer : '周杰伦',
+        audio : '/music/data/dnxk.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/等你下课.lrc'
+    },
+    {
+        title : '说好的幸福呢',
+        singer : '周杰伦',
+        audio : '/music/data/shd.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/说好的幸福呢.lrc'
+    },
+    {
+        title : '算什么男人',
+        singer : '周杰伦',
+        audio : '/music/data/ssm.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/算什么男人.lrc'
+    },
+    {
+        title : '明明就',
+        singer : '周杰伦',
+        audio : '/music/data/mmj.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/明明就.lrc'
+    },
+    {
+        title : '七里香',
+        singer : '周杰伦',
+        audio : '/music/data/qlx.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/七里香.lrc'
+    },
+    {
+        title : '烟花易冷',
+        singer : '周杰伦',
+        audio : '/music/data/yhyl.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/烟花易冷.lrc'
+    },
+    {
+        title : '红尘客栈',
+        singer : '周杰伦',
+        audio : '/music/data/hckz.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/红尘客栈.lrc'
+    },
+    {
+        title : '简单爱',
+        singer : '周杰伦',
+        audio : '/music/data/jda.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/简单爱.lrc'
+    },
+    {
+        title : '给我一首歌的时间',
+        singer : '周杰伦',
+        audio : '/music/data/gwy.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/给我一首歌的时间.lrc'
+    },
+    {
+        title : '黑色毛衣',
+        singer : '周杰伦',
+        audio : '/music/data/hsmy.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/黑色毛衣.lrc'
+    },
+    {
+        title : '最长的电影',
+        singer : '周杰伦',
+        audio : '/music/data/zcd.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/最长的电影.lrc'
+    },
+    {
+        title : '发如雪',
+        singer : '周杰伦',
+        audio : '/music/data/frx.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/发如雪.lrc'
+    },
+    {
+        title : '不能说的秘密',
+        singer : '周杰伦',
+        audio : '/music/data/bns.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/不能说的秘密.lrc'
+    },
+    {
+        title : '彩虹',
+        singer : '周杰伦',
+        audio : '/music/data/ch.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/彩虹.lrc'
+    },
+    {
+        title : '听妈妈的话',
+        singer : '周杰伦',
+        audio : '/music/data/tmm.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/听妈妈的话.lrc'
+    },
+    {
+        title : '夜曲',
+        singer : '周杰伦',
+        audio : '/music/data/yq.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/夜曲.lrc'
+    },
+    {
+        title : '甜甜的',
+        singer : '周杰伦',
+        audio : '/music/data/ttd.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/甜甜的.lrc'
+    },
+    {
+        title : '半岛铁盒',
+        singer : '周杰伦',
+        audio : '/music/data/bdth.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/半岛铁盒.lrc'
+    },
+    {
+        title : '东风破',
+        singer : '周杰伦',
+        audio : '/music/data/dfp.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/东风破.lrc'
+    },
+    {
+        title : '蒲公英的约定',
+        singer : '周杰伦',
+        audio : '/music/data/pgy.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/蒲公英的约定.lrc'
+    },
+    {
+        title : '一路向北',
+        singer : '周杰伦',
+        audio : '/music/data/ylxb.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/一路向北.lrc'
+    },
+    {
+        title : '菊花台',
+        singer : '周杰伦',
+        audio : '/music/data/jht.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/菊花台.lrc'
+    },
+    {
+        title : '珊瑚海',
+        singer : '周杰伦',
+        audio : '/music/data/shh.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/珊瑚海.lrc'
+    },
+    {
+        title : '星晴',
+        singer : '周杰伦',
+        audio : '/music/data/xq.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/星晴.lrc'
+    },
+    {
+        title : '霍元甲',
+        singer : '周杰伦',
+        audio : '/music/data/hyj.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/霍元甲.lrc'
+    },
+    {
+        title : '爱在西元前',
+        singer : '周杰伦',
+        audio : '/music/data/azx.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/爱在西元前.lrc'
+    },
+    {
+        title : '以父之名',
+        singer : '周杰伦',
+        audio : '/music/data/yfzm.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/以父之名.lrc'
+    },
+    {
+        title : '可爱女人',
+        singer : '周杰伦',
+        audio : '/music/data/kanr.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/可爱女人.lrc'
+    },
+    {
+        title : '园游会',
+        singer : '周杰伦',
+        audio : '/music/data/yyh.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/园游会.lrc'
+    },
+    {
+        title : '本草纲目',
+        singer : '周杰伦',
+        audio : '/music/data/bcgm.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/本草纲目.lrc'
+    },
+    {
+        title : '开不了口',
+        singer : '周杰伦',
+        audio : '/music/data/kblk.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/开不了口.lrc'
+    },
+    {
+        title : '退后',
+        singer : '周杰伦',
+        audio : '/music/data/th.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/退后.lrc'
+    },
+    {
+        title : '爱情废柴',
+        singer : '周杰伦',
+        audio : '/music/data/aqfc.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/爱情废柴.lrc'
+    },
+    {
+        title : '鞋子特大号',
+        singer : '周杰伦',
+        audio : '/music/data/xz.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/鞋子特大号.lrc'
+    },
+    {
+        title : '我不配',
+        singer : '周杰伦',
+        audio : '/music/data/wbp.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/我不配.lrc'
+    },
+    {
+        title : '哪里都是你',
+        singer : '周杰伦',
+        audio : '/music/data/nl.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/哪里都是你.lrc'
+    },
+    {
+        title : '借口',
+        singer : '周杰伦',
+        audio : '/music/data/jk.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/借口.lrc'
+    },
+    {
+        title :'夜的第七章',
+        singer : '周杰伦',
+        audio : '/music/data/ydd.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/夜的第七章.lrc'
+    },
+    {
+        title :'听见下雨的声音',
+        singer : '周杰伦',
+        audio : '/music/data/tjx.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/听见下雨的声音.lrc'
+    },
+    {
+        title : '听爸爸的话',
+        singer : '周杰伦',
+        audio : '/music/data/tbb.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/听爸爸的话.lrc'
+    },
+    {
+        title : '超人不会飞',
+        singer : '周杰伦',
+        audio : '/music/data/cr.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/超人不会飞.lrc'
+    },
+    {
+        title : '爷爷泡的茶',
+        singer : '周杰伦',
+        audio : '/music/data/yyp.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/爷爷泡的茶.lrc'
+    },
+    {
+        title : '屋顶',
+        singer : '周杰伦',
+        audio : '/music/data/wd.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/屋顶.lrc'
+    },
+    {
+        title : '三年二班',
+        singer : '周杰伦',
+        audio : '/music/data/snr.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/三年二班.lrc'
+    },
+    {
+        title : '龙战骑士',
+        singer : '周杰伦',
+        audio : '/music/data/lzqs.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/龙战骑士.lrc'
+    },
+    {
+        title : '雨下一整晚',
+        singer : '周杰伦',
+        audio : '/music/data/rain.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/雨下一整晚.lrc'
+    },
+    {
+        title : '最后的战役',
+        singer : '周杰伦',
+        audio : '/music/data/zhd.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/最后的战役.lrc'
+    },
+    {
+        title : '半兽人',
+        singer : '周杰伦',
+        audio : '/music/data/bsr.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/半兽人.lrc'
+    },
+    {
+        title : '她的睫毛',
+        singer : '周杰伦',
+        audio : '/music/data/tdjm.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/她的睫毛.lrc'
+    },
+    {
+        title : '对不起',
+        singer : '周杰伦',
+        audio : '/music/data/dbq.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/对不起.lrc'
+    },
+    {
+        title : '龙拳',
+        singer : '周杰伦',
+        audio : '/music/data/lq.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/龙拳.lrc'
+    },
+    {
+        title : '我落泪情绪零碎',
+        singer : '周杰伦',
+        audio : '/music/data/wll.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/我落泪情绪零碎.lrc'
+    },
+    {
+        title : '威廉古堡',
+        singer : '周杰伦',
+        audio : '/music/data/wlgb.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/威廉古堡.lrc'
+    },
+    {
+        title : '阳光宅男',
+        singer : '周杰伦',
+        audio : '/music/data/ygzn.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/阳光宅男.lrc'
+    },
+    {
+        title : '止战之殇',
+        singer : '周杰伦',
+        audio : '/music/data/zzzs.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/止战之殇.lrc'
+    },
+    {
+        title : '麦芽糖',
+        singer : '周杰伦',
+        audio : '/music/data/myt.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/麦芽糖.lrc'
+    },
+    {
+        title : '米兰小铁匠',
+        singer : '周杰伦',
+        audio : '/music/data/ml.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/米兰的小铁匠.lrc'
+    },
+    {
+        title : '反方向的钟',
+        singer : '周杰伦',
+        audio : '/music/data/ffx.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/反方向的钟.lrc'
+    },
+    {
+        title : '白色风车',
+        singer : '周杰伦',
+        audio : '/music/data/bsfc.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/白色风车.lrc'
+    },
+    {
+        title : '说好不哭',
+        singer : '周杰伦',
+        audio : '/music/data/shbk.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/说好不哭.lrc'
+    },
+    {
+        title : '手写的从前',
+        singer : '周杰伦',
+        audio : '/music/data/sxd.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/手写的从前.lrc'
+    },
+    {
+        title : '枫',
+        singer : '周杰伦',
+        audio : '/music/data/f.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/枫.lrc'
+    },
+    {
+        title : '花海',
+        singer : '周杰伦',
+        audio : '/music/data/hh.mp3',
+        thumbnail : '/music/data/pic.png',
+        lyric : '/Newmusic/data/花海.lrc'
+    },
+
+
+
+];
